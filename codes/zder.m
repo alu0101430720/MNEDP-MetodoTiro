719654840114fb3503c154390381677f19968282
alu0101430720 <alu0101430720@ull.edu.es>
2025-03-21 20:04:48 +0000

    Delete codes/zder.m

--- codes/zder.m
+++
@@ -1,8 +0,0 @@
-% MIT License
-% Copyright (c) 2025 Carlos Yanes Pérez
-% https://github.com/alu0101430720/MNEDP-MetodoTiro/tree/main
-
-function g = zder(t, z, y)
-    % G
-    g = [z(2); -2*(y(1)*z(2) + y(2)*z(1))/t];
-end
